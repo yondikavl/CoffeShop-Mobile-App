b056ddd358efb7d939cbf5308e23592d14235e2c
Yondika Vio Landa <yondikaviolanda@gmail.com>
2024-11-07 13:56:54 +0700

    feat(category, sectiontext)

--- app/src/main/java/com/example/coffeshop_mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/coffeshop_mobile_app/MainActivity.kt
@@ -1,26 +1,34 @@
 package com.example.coffeshop_mobile_app
 
+import CategoryItem
 import Search
+import SectionText
 import android.bluetooth.BluetoothClass.Device
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.coffeshop_mobile_app.model.dummyCategory
 import com.example.coffeshop_mobile_app.ui.theme.CoffeShopMobileAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,6 +47,8 @@ class MainActivity : ComponentActivity() {
 fun CoffeShopMobileApp() {
     Column {
         Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
     }
 }
 
@@ -57,10 +67,32 @@ fun Banner(
     }
 }
 
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory}) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
 @Preview(showBackground = true, device = Devices.PIXEL)
 @Composable
 fun CoffeShopMobileAppPreview() {
     CoffeShopMobileAppTheme {
         CoffeShopMobileApp()
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryRowPreview() {
+    CoffeShopMobileAppTheme {
+        CategoryRow()
     }
 }
