2e2b6b12de1a78e74241452bda881a704827436c
Yondika Vio Landa <yondikaviolanda@gmail.com>
2024-11-07 13:27:09 +0700

    feat(search): add composable banner and search

--- app/src/main/java/com/example/coffeshop_mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/coffeshop_mobile_app/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.coffeshop_mobile_app
 
+import Search
 import android.bluetooth.BluetoothClass.Device
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.coffeshop_mobile_app.ui.theme.CoffeShopMobileAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +37,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CoffeShopMobileApp() {
+    Column {
+        Banner()
+    }
+}
 
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        Search()
+    }
 }
 
 @Preview(showBackground = true, device = Devices.PIXEL)
